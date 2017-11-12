694d8a35ffd7e22eca5f157932299f567a9eb5ec
nodox <snatera09@gmail.com>
2017-11-12 15:53:17 -0500

    [Feat] Add sass support
    The website can support sass which means we can incorporate
    any css framework that works with sass.

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -3,30 +3,13 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
-import './index.css'
+import './index.scss'
 
 const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
+  <div>
+    <div>
+      <h1>
+        <Link to="/">
           Gatsby
         </Link>
       </h1>
@@ -44,14 +27,7 @@ const TemplateWrapper = ({ children }) => (
       ]}
     />
     <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <div className="nodox">
       {children()}
     </div>
   </div>
