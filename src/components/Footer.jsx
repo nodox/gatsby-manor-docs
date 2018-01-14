90a9ca3f398b5c20ebfe0c89df7a49a2a905e59e
nodox <snatera09@gmail.com>
2018-01-14 16:12:08 -0500

    [Chore] clean up footer
    Remove dead code as well as dead logic.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,29 +6,11 @@ import "../styles/footer.scss";
 export default class Footer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      enableSticky: false,
-      path: this.props.path,
-    }
-  }
-
-  componentWillReceiveProps(nextProps) {
-    this.setState((prevState, nextProps) => {
-      let enable = nextProps.path.includes('/docs/cli/')
-                    || nextProps.path.includes('/docs')
-                    || nextProps.path.includes('/community');
-
-      return {
-        path: nextProps.path,
-        enableSticky: false,
-      };
-    });
   }
 
   render() {
     let footerClass = classNames({
       'footer--container': true,
-      'footer--container--sticky': this.state.enableSticky,
     });
 
     return (
@@ -37,6 +19,4 @@ export default class Footer extends React.Component {
       </div>
     );
   }
-
-
 }
